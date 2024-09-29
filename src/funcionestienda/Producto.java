f5fdb0bc7db958c826ac375ce8dd3750777ac61c
MKULTRA\josue <josuedelgadonieto@gmail.com>
2024-09-28 20:55:08 -0600

    no message

---
+++ src/funcionestienda/Producto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package funcionestienda;
+public class Producto {
+    String descripcion;
+    double precio;
+
+    public Producto(String descripcion, double precio) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
